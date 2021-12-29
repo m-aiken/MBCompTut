14ceb025c8ce0f2a49fc95cb78a77fc8e11b79a1
m-aiken <mattaiken91@gmail.com>
2021-12-29 15:54:08 +0000

    added compressor

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -58,6 +58,8 @@ public:
     
     APVTS apvts { *this, nullptr, "Parameters", createParameterLayout() };
 private:
+    juce::dsp::Compressor<float> compressor;
+    
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MBCompTutorialAudioProcessor)
 };

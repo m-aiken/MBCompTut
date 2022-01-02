6f97cf47f282b6dcc04ef351d874254e737b0f68
m-aiken <mattaiken91@gmail.com>
2022-01-02 15:48:27 +0000

    added Band class

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -14,56 +14,18 @@ MBCompTutorialAudioProcessorEditor::MBCompTutorialAudioProcessorEditor (MBCompTu
     : AudioProcessorEditor (&p), audioProcessor (p),
       gainInput_Attachment(audioProcessor.apvts, "Gain In", gainInput),
       gainOutput_Attachment(audioProcessor.apvts, "Gain Out", gainOutput),
-      thresholdL_Attachment(audioProcessor.apvts, "Threshold Low Band", thresholdL),
-      attackL_Attachment(audioProcessor.apvts, "Attack Low Band", attackL),
-      releaseL_Attachment(audioProcessor.apvts, "Release Low Band", releaseL),
-      thresholdM_Attachment(audioProcessor.apvts, "Threshold Mid Band", thresholdM),
-      attackM_Attachment(audioProcessor.apvts, "Attack Mid Band", attackM),
-      releaseM_Attachment(audioProcessor.apvts, "Release Mid Band", releaseM),
-      thresholdH_Attachment(audioProcessor.apvts, "Threshold High Band", thresholdH),
-      attackH_Attachment(audioProcessor.apvts, "Attack High Band", attackH),
-      releaseH_Attachment(audioProcessor.apvts, "Release High Band", releaseH),
       crossoverLowMid_Attachment(audioProcessor.apvts, "Low-Mid Crossover Frequency", crossoverLowMid),
-      crossoverMidHigh_Attachment(audioProcessor.apvts, "Mid-High Crossover Frequency", crossoverMidHigh),
-      bypassL_Attachment(audioProcessor.apvts, "Bypass Low Band", bypassL),
-      muteL_Attachment(audioProcessor.apvts, "Mute Low Band", muteL),
-      soloL_Attachment(audioProcessor.apvts, "Solo Low Band", soloL),
-      bypassM_Attachment(audioProcessor.apvts, "Bypass Mid Band", bypassM),
-      muteM_Attachment(audioProcessor.apvts, "Mute Mid Band", muteM),
-      soloM_Attachment(audioProcessor.apvts, "Solo Mid Band", soloM),
-      bypassH_Attachment(audioProcessor.apvts, "Bypass High Band", bypassH),
-      muteH_Attachment(audioProcessor.apvts, "Mute High Band", muteH),
-      soloH_Attachment(audioProcessor.apvts, "Solo High Band", soloH)
+      crossoverMidHigh_Attachment(audioProcessor.apvts, "Mid-High Crossover Frequency", crossoverMidHigh)
 {
-    addRotary(thresholdL, thresholdL_Label, "Threshold");
-    addRotary(thresholdM, thresholdM_Label, "Threshold");
-    addRotary(thresholdH, thresholdH_Label, "Threshold");
-    
-    addRotary(attackL, attackL_Label, "Attack");
-    addRotary(attackM, attackM_Label, "Attack");
-    addRotary(attackH, attackH_Label, "Attack");
-    
-    addRotary(releaseL, releaseL_Label, "Release");
-    addRotary(releaseM, releaseM_Label, "Release");
-    addRotary(releaseH, releaseH_Label, "Release");
-    
     addRotary(crossoverLowMid, crossoverLowMid_Label, "Low-Mid Crossover");
     addRotary(crossoverMidHigh, crossoverMidHigh_Label, "Mid-High Crossover");
     
     addRotary(gainInput, gainInput_Label, "Gain In");
     addRotary(gainOutput, gainOutput_Label, "Gain Out");
     
-    addToggle(bypassL, "X");
-    addToggle(bypassM, "X");
-    addToggle(bypassH, "X");
-    
-    addToggle(muteL, "M");
-    addToggle(muteM, "M");
-    addToggle(muteH, "M");
-    
-    addToggle(soloL, "S");
-    addToggle(soloM, "S");
-    addToggle(soloH, "S");
+    addAndMakeVisible(lowBand);
+    addAndMakeVisible(midBand);
+    addAndMakeVisible(highBand);
     
     setSize (600, 450);
 }
@@ -82,31 +44,10 @@ void MBCompTutorialAudioProcessorEditor::resized()
 {
     auto bounds = getLocalBounds();
     auto rightStrip = bounds.removeFromRight(150);
-  
-    auto low = bounds.removeFromTop(150);
-    auto lowToggles = low.removeFromRight(50);
     
-    auto mid = bounds.removeFromTop(150);
-    auto midToggles = mid.removeFromRight(50);
-    
-    auto high = bounds.removeFromTop(150);
-    auto highToggles = high.removeFromRight(50);
-    
-    // Low band
-    auto lowControls = buildBand(thresholdL, attackL, releaseL);
-    lowControls.performLayout(low);
-    auto lToggles = buildToggles(bypassL, muteL, soloL);
-    lToggles.performLayout(lowToggles);
-    // Mid band
-    auto midControls = buildBand(thresholdM, attackM, releaseM);
-    midControls.performLayout(mid);
-    auto mToggles = buildToggles(bypassM, muteM, soloM);
-    mToggles.performLayout(midToggles);
-    // High band
-    auto highControls = buildBand(thresholdH, attackH, releaseH);
-    highControls.performLayout(high);
-    auto hToggles = buildToggles(bypassH, muteH, soloH);
-    hToggles.performLayout(highToggles);
+    lowBand.setBounds(bounds.removeFromTop(150));
+    midBand.setBounds(bounds.removeFromTop(150));
+    highBand.setBounds(bounds);
 
     // Crossover / IO Gain
     using Track = juce::Grid::TrackInfo;
@@ -187,10 +128,4 @@ void MBCompTutorialAudioProcessorEditor::addRotary(juce::Slider& slider, juce::L
     label.setText(labelText, juce::dontSendNotification);
     label.setJustificationType(juce::Justification::centred);
     label.attachToComponent(&slider, false);
-}
-
-void MBCompTutorialAudioProcessorEditor::addToggle(juce::ToggleButton& button, const juce::String& buttonText)
-{
-    addAndMakeVisible(button);
-    button.setButtonText(buttonText);
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -11,6 +11,107 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
+struct Band : juce::Component
+{
+    juce::Slider thresholdRotary{ juce::Slider::SliderStyle::Rotary, juce::Slider::TextEntryBoxPosition::TextBoxBelow },
+                 attackRotary{ juce::Slider::SliderStyle::Rotary, juce::Slider::TextEntryBoxPosition::TextBoxBelow },
+                 releaseRotary{ juce::Slider::SliderStyle::Rotary, juce::Slider::TextEntryBoxPosition::TextBoxBelow };
+    
+    juce::Label thresholdLabel{ "Threshold", "Threshold" },
+                attackLabel{ "Attack", "Attack" },
+                releaseLabel{ "Release", "Release" };
+    
+    juce::ToggleButton bypassButton{ "X" },
+                       muteButton{ "M" },
+                       soloButton{ "S" };
+    
+    using sliderAttachment = juce::AudioProcessorValueTreeState::SliderAttachment;
+    sliderAttachment thresholdAttachment, attackAttachment, releaseAttachment;
+    
+    using buttonAttachment = juce::AudioProcessorValueTreeState::ButtonAttachment;
+    buttonAttachment bypassAttachment, muteAttachment, soloAttachment;
+    
+    Band(MBCompTutorialAudioProcessor& p, const juce::String& bandName) :
+        thresholdAttachment(p.apvts, ("Threshold " + bandName + " Band"), thresholdRotary),
+        attackAttachment(p.apvts, ("Attack " + bandName + " Band"), attackRotary),
+        releaseAttachment(p.apvts, ("Release " + bandName + " Band"), releaseRotary),
+        bypassAttachment(p.apvts, ("Bypass " + bandName + " Band"), bypassButton),
+        muteAttachment(p.apvts, ("Mute " + bandName + " Band"), muteButton),
+        soloAttachment(p.apvts, ("Solo " + bandName + " Band"), soloButton)
+    {
+        addAndMakeVisible(thresholdRotary);
+        addAndMakeVisible(attackRotary);
+        addAndMakeVisible(releaseRotary);
+        
+        addAndMakeVisible(thresholdLabel);
+        addAndMakeVisible(attackLabel);
+        addAndMakeVisible(releaseLabel);
+        
+        thresholdLabel.setJustificationType(juce::Justification::centred);
+        attackLabel.setJustificationType(juce::Justification::centred);
+        releaseLabel.setJustificationType(juce::Justification::centred);
+        
+        addAndMakeVisible(bypassButton);
+        addAndMakeVisible(muteButton);
+        addAndMakeVisible(soloButton);
+    }
+    
+    // dark blue = juce::Colour(13u, 17u, 23u);
+    // light grey = juce::Colour(48u, 54u, 61u);
+    void paint(juce::Graphics& g) override
+    {
+        g.fillAll(juce::Colour(13u, 17u, 23u));
+    }
+    
+    void resized() override
+    {
+        auto bounds = getLocalBounds();
+        auto toggleContainer = bounds.removeFromRight(50);
+        auto labelContainer = bounds.removeFromTop(10);
+        
+        using Track = juce::Grid::TrackInfo;
+        using Fr = juce::Grid::Fr;
+        
+        // Labels
+        juce::Grid labels;
+        labels.templateRows = { Track(Fr (1)) };
+        labels.templateColumns = { Track(Fr (1)), Track(Fr (1)), Track(Fr (1)) };
+        
+        labels.items = { juce::GridItem(thresholdLabel), juce::GridItem(attackLabel), juce::GridItem(releaseLabel) };
+        labels.performLayout(labelContainer);
+        
+        // Rotaries - Threshold / Attack / Release
+        juce::Grid rotaries;
+        rotaries.templateRows = { Track(Fr (1)) };
+        rotaries.templateColumns = { Track(Fr (1)), Track(Fr (1)), Track(Fr (1)) };
+        
+        auto t = juce::GridItem(thresholdRotary);
+        auto a = juce::GridItem(attackRotary);
+        auto r = juce::GridItem(releaseRotary);
+        
+        int mrg = 20;
+        t.margin = mrg;
+        a.margin = mrg;
+        r.margin = mrg;
+        
+        rotaries.items = { juce::GridItem(t), juce::GridItem(a), juce::GridItem(r) };
+        rotaries.performLayout(bounds);
+        
+        // Toggle buttons
+        juce::Grid toggles;
+        toggles.templateRows = { Track(Fr (1)), Track(Fr (1)), Track(Fr (1)) };
+        toggles.templateColumns = { Track(Fr (1)) };
+        
+        toggles.items = {
+            juce::GridItem(bypassButton),
+            juce::GridItem(muteButton),
+            juce::GridItem(soloButton)
+        };
+        
+        toggles.performLayout(toggleContainer);
+    }
+};
+
 //==============================================================================
 /**
 */
@@ -28,45 +129,28 @@ public:
     juce::Grid buildToggles(juce::ToggleButton& bypass, juce::ToggleButton& mute, juce::ToggleButton& solo);
     
     void addRotary(juce::Slider& slider, juce::Label& label, const juce::String& labelText);
-    void addToggle(juce::ToggleButton& button, const juce::String& buttonText);
 
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     MBCompTutorialAudioProcessor& audioProcessor;
-        
+    
+    Band lowBand{ audioProcessor, "Low" };
+    Band midBand{ audioProcessor, "Mid" };
+    Band highBand{ audioProcessor, "High" };
+    
     juce::Slider gainInput, gainOutput;
     
     juce::Label gainInput_Label, gainOutput_Label;
-    
-    juce::Slider thresholdL, attackL, releaseL,
-                 thresholdM, attackM, releaseM,
-                 thresholdH, attackH, releaseH;
-    
-    juce::Label  thresholdL_Label, attackL_Label, releaseL_Label,
-                 thresholdM_Label, attackM_Label, releaseM_Label,
-                 thresholdH_Label, attackH_Label, releaseH_Label;
     
     juce::Slider crossoverLowMid, crossoverMidHigh;
     
     juce::Label crossoverLowMid_Label, crossoverMidHigh_Label;
     
-    juce::ToggleButton bypassL, muteL, soloL,
-                       bypassM, muteM, soloM,
-                       bypassH, muteH, soloH;
-    
     using sliderAttachment = juce::AudioProcessorValueTreeState::SliderAttachment;
     
     sliderAttachment gainInput_Attachment, gainOutput_Attachment,
-                     thresholdL_Attachment, attackL_Attachment, releaseL_Attachment,
-                     thresholdM_Attachment, attackM_Attachment, releaseM_Attachment,
-                     thresholdH_Attachment, attackH_Attachment, releaseH_Attachment,
                      crossoverLowMid_Attachment, crossoverMidHigh_Attachment;
-    
-    using buttonAttachment = juce::AudioProcessorValueTreeState::ButtonAttachment;
-    buttonAttachment bypassL_Attachment, muteL_Attachment, soloL_Attachment,
-                     bypassM_Attachment, muteM_Attachment, soloM_Attachment,
-                     bypassH_Attachment, muteH_Attachment, soloH_Attachment;
     
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MBCompTutorialAudioProcessorEditor)
 };
